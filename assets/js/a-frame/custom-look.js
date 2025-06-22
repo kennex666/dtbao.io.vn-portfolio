f2f78315ffd4c858004aca2057522119d315164b
Dương Thái Bảo <me@dtbao.io.vn>
2025-06-23 00:50:58 +0700

    update: attribute for look controll, feat shader default, change color for object

--- assets/js/a-frame/custom-look.js
+++ assets/js/a-frame/custom-look.js
@@ -19,3 +19,44 @@ AFRAME.registerComponent("custom-look", {
 		}
 	},
 });
+
+AFRAME.registerComponent("custom-color", {
+	schema: {
+		color: { type: "string", default: "#d6a84f" },
+	},
+	init: function () {
+		this.el.addEventListener("model-loaded", () => {
+			const mesh = this.el.getObject3D("mesh");
+			if (!mesh) return;
+
+			mesh.traverse((node) => {
+				if (node.isMesh && node.material) {
+					node.material.color.set(this.data.color); // màu vàng dịu
+					node.material.roughness = 0.7; // bớt bóng
+					node.material.metalness = 0.1; // hơi có phản sáng
+					node.material.needsUpdate = true;
+				}
+			});
+		});
+	},
+});
+
+AFRAME.registerComponent("force-flat-shader", {
+	init: function () {
+		this.el.addEventListener("model-loaded", () => {
+			const model = this.el.getObject3D("mesh");
+			if (!model) return;
+
+			model.traverse((node) => {
+				if (node.isMesh) {
+					node.material = new THREE.MeshBasicMaterial({
+						map: node.material.map, // giữ texture cũ nếu có
+						color: node.material.color,
+						transparent: node.material.transparent,
+						opacity: node.material.opacity,
+					});
+				}
+			});
+		});
+	},
+});
